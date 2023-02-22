b34d8f0c4e995d690d91f182e932857fa5b651c8
Sunilkumar S H <72041375+Sunilkumar-S-Halasur@users.noreply.github.com>
2023-02-22 11:41:10 +0530

    Update settings.py

--- jwt_djoser/settings.py
+++ jwt_djoser/settings.py
@@ -167,5 +167,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'sunish096@gmail.com'
-EMAIL_HOST_PASSWORD = 'qwfcbvnwwxomlkxd'
+EMAIL_HOST_USER = 'your email id'
+EMAIL_HOST_PASSWORD = 'your Password '
